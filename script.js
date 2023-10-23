2f2b899c63b157204124231abefba79461d4ef59
hl5667 <144741454+hl5667@users.noreply.github.com>
2023-10-23 19:18:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,151 +1,151 @@
-// let bg = [0, 0, 0]
-
-// //method 1
-// function callFirst() {
-//   console.log("hello, delayed function")
-//   bg = [50, 100, 100]
-// };
-// setTimeout(callFirst, 2000)
 
 
-// //method 2 
-// setTimeout(
-//   function() {
-//       console.log("hello, again!")
-//   bg = [100, 100, 100]
-//   }, 4000);
+class CircleA {
+  constructor() {
+    this.x = random(0, 1000);
+    this.y = random(0, 1000);
+    this.xspeed = 3;
+    this.yspeed = 3;
+    this.r = random(100, 500);
+    this.rspeed = 2;
+    
+  }
 
-// //method 3 - arrow syntax
-// setTimeout( () => console.log ("hello, yet again!"), 6000)
+  display(start) {
+    let hue = chroma.hsl((start+frameCount)%360, 1, 0.5)
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+    this.r += this.rspeed;
 
-// setTimeout( () => {
-//   console.log ("hello, again and again!");
-//   bg = [200,100,200]
-// }, 9000)
+    if (this.x > 1000 || this.x < 0) {
+      this.xspeed *= -1;
+    }
+    
+    if (this.y > 1000 || this.y < 0) {
+      this.yspeed *= -1;
+    }
 
+    if (this.r > 800) {
+      this.rspeed *= -1;
+    }
+    
+    if (this.r < 100) {
+      this.rspeed *= -1; 
+    }
 
-// //these 3 work the same, just different ways of writing it. 
-// function setup() {
-//   createCanvas(600, 600)
-// }
+    fill(hue.rgb())
+    ellipse(this.x, this.y, this.r);
+  }
+}
 
-// function draw() {
-//   background(bg)
-// }
+class CircleB {
+  constructor(r) {
+    this.x = random(0, 1000);
+    this.y = random(0, 1000);
+    this.xspeed = 3;
+    this.yspeed = 3;
+    this.r = random(100, 500);
+    this.rspeed = 2;
+  }
 
+  display(start) {
+    let hue = chroma.hsl((start+frameCount)%360, 1, 0.5)
+    this.x -= this.xspeed;
+    this.y -= this.yspeed;
+    this.r -= this.rspeed;
 
+    if (this.x > 1000 || this.x < 0) {
+      this.xspeed *= -1;
+    }
+    
+    if (this.y > 1000 || this.y < 0) {
+      this.yspeed *= -1;
+    }
 
+    if (this.r > 800) {
+      this.rspeed *= -1;
+    }
+    
+    if (this.r < 100) {
+      this.rspeed *= -1; // Corrected from this.yspeed to this.rspeed
+    }
 
+    ellipse(this.x, this.y, this.r);
 
-// NEW CODE STARTS HERE 
-
-let imgURL
-let myImage 
+  }
+}
 
 function setup() {
-  createCanvas(600, 600);
-  apiRequest();
+createCanvas(1000, 1000);
+COne = new CircleA();
+CTwo = new CircleB();
+CThree = new CircleA();
+CFour = new CircleB();
+CFive = new CircleA();
+CSix = new CircleB();
+CSeven = new CircleA();
+CEight = new CircleB();
+CNine = new CircleA();
+CTen = new CircleB();
+
+
 }
 
 function draw() {
-  background(100);
-  if(myImage != undefined) {
-  image(myImage, 0, 0)  
-  }
-  
+hue = chroma.hsl(0.5*frameCount%360, 1, 0.5)
+
+background(hue.rgb());
+
+COne.display(30);
+CTwo.display(250);
+CThree.display(100);
+CFour.display(120);
+CFive.display(300);
+CSix.display(0);
+CSeven.display(60);
+CEight.display(90);
+CNine.display(280);
+CTen.display(190);
+
+
+drawingContext.filter = 'blur(300px)'
+// drawingContext.filter = 'contrast(100)'
+
+
 }
 
-async function apiRequest() { 
-  let request = await fetch
-  ("https://dog.ceo/api/breeds/image/random")
-
-  console.log(request)
-
-  let data = await request.json()
-  console.log(data)
-
-  imgURL = data.message 
-  console.log(imgURL)
-
-  myImage = loadImage(imgURL)
-}
-
-
-
-
-
-
-
-
-
-// NEW NEW CODE STARTS HERE
-
-// let maxTemps
-// let minTemps
-
-// function setup() {
-//   createCanvas(600, 600);
-//   apiRequest();
-// }
-
-// function draw() {
-//   background(100);
-//   if(myImage != undefined) {
-//   image(myImage, 0, 0)  
-//   }
-
-//   "!=" is un-equal
-
-  // if (maxTemps != undefined){
-  //   let h = map(maxTemps[0],0,100,height, 0)
-  //   rect(0, h,100, 100) 
-  // }
-
-
-//   if(maxTemps != undefined) {
-//     for(let i = 0; i < maxTemps.length; i++) {
-//       let h = map(maxTemps[i],0,100,height, 0)
-//       rect(i*100, h,100, 100)
+// function showLens(x, y) {
+//   // Lens properties
+//   let lsize = 60, lsize2 = lsize * lsize;
+//   let mag = 2.0;
+//   let k = -0.00016;
+//   var u, v, r2;
+//   loadPixels();
+//   for (let vd = - lsize; vd < lsize; vd++) {
+//     for (let ud = - lsize; ud < lsize; ud++) {
+//       r2 = ud*ud + vd*vd;
+//       console.log(vd,ud)
+//       if (r2 <= lsize2) {
+//         let f = mag + k * r2;
+//         u = floor(ud/f) + x - offX;
+//         v = floor(vd/f) + y - offY;
+//         let px = ud + x;
+//         let py = vd + y;
+//         if (px >=0 && px < 1000 && py >=0 && py < 1000) {
+//           if (u >= 0 && u < 1000 && v >=0 && v < 1000) {
+//             set(ud + x, vd + y, color(255,0,0));
+//           } else {
+//             set(ud + x, vd + y, color(180));
+//           }
+//         }
+//       }
 //     }
 //   }
-
-//   if(minTemps != undefined) {
-//     for(let i = 0; i < minTemps.length; i++) {
-//       let h = map(minTemps[i],0,100,height, 0)
-//       rect(i*100, h,100, 100)
-//     }
-//   }
-
-  
-
-
-  
+//   updatePixels();
+// }
+// function mouseClicked() {
+// // Handle mouse click event if needed
 // }
 
-// async function apiRequest() { 
-//   let request = await fetch
-//   ("https://api.open-meteo.com/v1/forecast?latitude=-18&longitude=178&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FAnchorage")
-
-//   console.log(request)
-
-//   let data = await request.json()
-//   console.log(data)
-
-//   let dailyTemps = data.daily;
-//   console.log(dailyTemps)
-
-//   maxTemps = dailyTemps.temperature_2m_max;
-//   console.log(maxTemps)
-
-//   minTemps = dailyTemps.temperature_2m_min;
-//   console.log(minTemps)
 
 
-
-
-
-//   imgURL = data.message 
-//   console.log(imgURL)
-
-//   myImage = loadImage(imgURL)
-// }
